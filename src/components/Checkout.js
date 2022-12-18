70a0d590753ef9186182c210528ae6f325ea3ef6
HotCheetos101 <120177674+HotCheetos101@users.noreply.github.com>
2022-12-18 20:50:39 +0800

    added pop up

--- src/components/Checkout.js
+++ src/components/Checkout.js
@@ -5,76 +5,108 @@ import CartItem from './CartItem';
 import '../CSS/Oncet.css';
 
 const Checkout = ({
-  products,
-  setShowCheckout,
-  show,
-  sm
-  }) => {
+    products,
+    setShowCheckout,
+    show,
+    sm
+}) => {
     const [sum, setSum] = useState(0);
 
- 
-    const [firstName,setFirstName]=useState('')
-    const [lastName,setLastName]=useState('')
-    const [error,setError]=useState(false)
+
+    const [firstName, setFirstName] = useState('')
+    const [lastName, setLastName] = useState('')
+    const [error, setError] = useState(false)
+
+    // ni add ta ug fucntion
+
+    let popup = document.getElementById("popup");
 
 
     const checkout = () => {
-        alert(`Checkout - Subtotal: ₱ ${sm.toFixed(2)}`);
+        // alert(`Checkout - Subtotal: ₱ ${sm.toFixed(2)}`);
+        openPopup();
     }
 
-    const handleSubmit=(e)=>{
+    const openPopup = () => {
+        popup.classList.add("open-popup");
+    }
+
+    const closePopup = () => {
+        popup.classList.remove("open-popup");
+    }
+
+
+    const handleSubmit = (e) => {
         e.preventDefault();
-        if(firstName.length==0||lastName.length==0){
+        if (firstName.length == 0 || lastName.length == 0) {
             setError(true)
         }
-        if(firstName&&lastName)
-        {
-        console.log("  Email: ",firstName,"\n  Password: ",lastName)
+        if (firstName && lastName) {
+            console.log("  Email: ", firstName, "\n  Password: ", lastName)
         }
     }
 
 
     return (
         show ? (
-          <div className="checkout-page">
-              { 
-                      <>
-           <form onSubmit={handleSubmit}>
-               <div className="info">Payment Info</div>
-               <div className="input-box">
-                   <input className="input-field" placeholder="Email" onChange={e=>setFirstName(e.target.value)} />
-               </div>
-               {error&&firstName.length<=0?
-               <label>First Name can't be Empty</label>:""}
-               <div className="input-box">
-                   <input className="input-field" placeholder="Address" onChange={e=>setLastName(e.target.value)} />
-               </div>
-               <div className="payment-mode">
-                   Mode of Payment:
-                   </div>
-                   <div>
-               <select>
-                        <option>Cash</option>
-                        <option>E-Wallet</option>
-                        <option>Card</option>
-                    </select>
-               </div>
-               {error&&lastName.length<=0?
-               <label>address can't be Empty</label>:""}
-               <div>
-                   <button onClick={error == true ? checkout : null} className="check-btn">
-                       Submit
-                   </button>
-               </div>
-           </form>
-        </>
-    }
-              <button onClick={() => setShowCheckout(false)} className="check-btn">Close</button>
-          </div>
-      ) : null
-      
-      )
-  
+            <div className="checkout-page">
+                {
+                    <>
+                        <form onSubmit={handleSubmit}>
+                            <div className="info">Payment Info</div>
+                            <div className="input-box">
+                                <input className="input-field" placeholder="Email" onChange={e => setFirstName(e.target.value)} />
+                            </div>
+                            {error && firstName.length <= 0 ?
+                                <label>First Name can't be Empty</label> : ""}
+                            <div className="input-box">
+                                <input className="input-field" placeholder="Address" onChange={e => setLastName(e.target.value)} />
+                            </div>
+                            <div className="payment-mode">
+                                Mode of Payment:
+                            </div>
+                            <div>
+                                <select>
+                                    <option>Cash</option>
+                                    <option>E-Wallet</option>
+                                    <option>Card</option>
+                                </select>
+                            </div>
+                            {error && lastName.length <= 0 ?
+                                <label>address can't be Empty</label> : ""}
+                            <div>
+                                <button onClick={error == true ? checkout : null} className="check-btn">
+                                    Submit
+                                </button>
+
+
+                                {/* experimental button */}
+                                <button onClick={error == true ? checkout : null} className="check-btn">
+                                    experiment Submit button
+                                </button>
+
+                                {/* laing way para pop up  */}
+
+                                <div class="popup" id='popup'>
+                                    <img src="https://i.ibb.co/b17yNf9/logo.png" className='logojapon' data-testid="logo" />
+                                    <h1>Checkout</h1>
+                                    <h2>Subtotal: ₱ ${sm.toFixed(2)}</h2>
+                                    <button type="button" onClick={() => closePopup()}>ok</button>
+                                </div>
+
+
+
+
+                            </div>
+                        </form>
+                    </>
+                }
+                <button onClick={() => setShowCheckout(false)} className="check-btn">Close</button>
+            </div>
+        ) : null
+
+    )
+
 }
 
 export default Checkout;
